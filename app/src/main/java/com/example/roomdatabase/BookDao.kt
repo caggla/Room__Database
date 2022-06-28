be97419f5fe71c31b761439f5e4b7619bf7ea900
caglacorakk <corakcagla021@gmail.com>
2022-06-28 04:39:31 +0300

    Room App

---
+++ app/src/main/java/com/example/roomdatabase/BookDao.kt
@@ -0,0 +1,19 @@
+package com.example.roomdatabase
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface BookDao {
+    @Insert
+    suspend fun insertBook(book: Book)
+
+    @Query("SELECT * FROM books_table")
+    fun getAllBooks(): List<Book>
+
+    @Update
+    suspend fun updateBook(book: Book)
+
+    @Delete
+    suspend fun deleteBook(book: Book)
+}
---
+++ app/src/main/java/com/example/roomdatabase/BookDatabase.kt
@@ -0,0 +1,9 @@
+package com.example.roomdatabase
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Book::class], version = 1)
+abstract class BookDatabase : RoomDatabase() {
+    abstract fun bookDao(): BookDao
+}
